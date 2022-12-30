54c7f0d9cb8d755a695bed71d1160663af3031a4
Zerohour43123 <69132724+Zerohour43123@users.noreply.github.com>
2022-12-30 11:23:03 -0700

    Further const of unasfe bitboard.rs items along with info rename and enabling of LTO

--- src/misc.rs
+++ src/misc.rs
@@ -34,7 +34,7 @@ impl Prng {
 pub fn engine_info(to_uci: bool) -> String {
 //    let months = &"Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec";
 
-    format!("Rustfish 10 dev{}",
+    format!("Rustfish 10 polish-reduce-unsafe dev{}",
             if to_uci {
                 "\nid author T. Romstad, m. Costalba, J. Kiiski, G. Linscott"
             } else {
